d1ea855e93da3cb97760cf4cf29f1c9606aa152c
권장순 <jsoonworld@gmail.com>
2025-04-12 23:48:31 +0900

    feat(DelimiterParser) : 문자열 입력에 포함된 "\\n"을 실제 개행 문자로 처리하도록 개선

--- src/main/kotlin/calculator/domain/DelimiterParser.kt
+++ src/main/kotlin/calculator/domain/DelimiterParser.kt
@@ -1,7 +1,27 @@
 package calculator.domain
 
-interface DelimiterParser {
-    fun supports(input: String): Boolean
-    fun extractDelimiter(input: String): String
-    fun extractNumbers(input: String): String
+object DelimiterParser {
+    private const val CUSTOM_PREFIX = "//"
+    private val DEFAULT_DELIMITER_REGEX = ",|:"
+
+    fun parse(input: String): Pair<String, String> {
+        return if (input.startsWith(CUSTOM_PREFIX)) {
+            parseCustomDelimiter(input)
+        } else {
+            DEFAULT_DELIMITER_REGEX to input
+        }
+    }
+
+    private fun parseCustomDelimiter(input: String): Pair<String, String> {
+        val normalizedInput = input.replace("\\n", "\n")
+
+        val endOfDelimiterIndex = normalizedInput.indexOf('\n')
+        if (endOfDelimiterIndex == -1) {
+            throw IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.")
+        }
+
+        val delimiter = normalizedInput.substring(CUSTOM_PREFIX.length, endOfDelimiterIndex)
+        val numbersPart = normalizedInput.substring(endOfDelimiterIndex + 1)
+        return Regex.escape(delimiter) to numbersPart
+    }
 }
